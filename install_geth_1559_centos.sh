5b3d16780e43959918da1fd1ba865580d095e1c9
Abdelhamid Bakhta <45264458+abdelhamidbakhta@users.noreply.github.com>
2021-01-20 09:47:37 +0100

    Update install_geth_1559_centos.sh

--- install_geth_1559_centos.sh
+++ install_geth_1559_centos.sh
@@ -4,6 +4,7 @@ wget https://dl.google.com/go/go1.13.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.13.linux-amd64.tar.gz
 sudo touch /etc/profile.d/go.sh
 echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee -a /etc/profile.d/go.sh
+source /etc/profile
 rm go1.13.linux-amd64.tar.gz
 git clone https://github.com/vulcanize/go-ethereum.git
 cd go-ethereum
